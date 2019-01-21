5e87ac61f551a867a5db4bf4c44dc272fbafe3c9
Skye Bleed <35306846+ithinkthisisarc@users.noreply.github.com>
2019-01-20 21:04:47 -0500

    fixes

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 -- DEPENDANCIES
-utils =  require( "src/utils" )
+utils = require( "src/utils" )
 kutils = require( "src/keyboard" )
 
 -- GLOBAL VARS
@@ -30,20 +30,6 @@ function love.update(deltatime)
             fireCounter = fireCounter - everyFire
         end
     end
-
-    for i=1, #bullets, 1 do
-        local bullet = bullets[i]
-        love.graphics.setColor(0, 0, 0)
-        if bullet.dire == "n" then
-            bullet.y = bullet.y - 5
-        elseif bullet.dire == "s" then
-            bullet.y = bullet.y + 5
-        elseif bullet.dire == "e" then
-            bullet.x = bullet.x + 5
-        elseif bullet.dire == "w" then
-            bullet.x = bullet.x - 5
-        end
-    end
 end
 
 function love.draw(dt)
@@ -51,6 +37,22 @@ function love.draw(dt)
 
     love.graphics.setColor(255, 0, 0)
     sprite = love.graphics.circle("fill", cX, cY, 20, 100)
-    
-    love.graphics.circle("fill", bullet.x, bullet.y, 7, 100)
+
+    for i=1, #bullets, 1 do
+        bullet = bullets[i]
+        love.graphics.setColor(0, 0, 0)
+        if bullet.dire == "n" then
+            bullet.y = bullet.y - 5
+            love.graphics.circle("fill", bullet.x, bullet.y, 7, 100)
+        elseif bullet.dire == "s" then
+            bullet.y = bullet.y + 5
+            love.graphics.circle("fill", bullet.x, bullet.y, 7, 100)
+        elseif bullet.dire == "e" then
+            bullet.x = bullet.x + 5
+            love.graphics.circle("fill", bullet.x, bullet.y, 7, 100)
+        elseif bullet.dire == "w" then
+            bullet.x = bullet.x - 5
+            love.graphics.circle("fill", bullet.x, bullet.y, 7, 100)
+        end
+    end
 end
