7feeddda0d16d6f122e6cc835487c680175f64e6
Skye Bleed <35306846+ithinkthisisarc@users.noreply.github.com>
2019-01-24 17:16:03 -0500

    Add files via upload

--- main.lua
+++ main.lua
@@ -20,6 +20,10 @@ fireCounter = 0
 everyFire = 0.2
 
  -- MAIN CHUNK
+function love.load()
+    world = love.physics.newWorld(20, 20, true)
+end
+
 function love.update(deltatime)
     kutils.check_keys() -- check for key events
 
@@ -38,9 +42,16 @@ function love.draw(dt)
     love.graphics.setColor(255, 0, 0)
     sprite = love.graphics.circle("fill", cX, cY, 20, 100)
 
+    if love.mouse.isDown(2) then
+        love.graphics.setColor(0, 0, 255)
+        utils.draw_sword(facing)
+    end
+
     for i=1, #bullets, 1 do
         local bullet = bullets[i]
         love.graphics.setColor(0, 0, 0)
         utils.draw_bullet(bullet)
     end
+
+    sprite = love.physics.newBody(world, 10, 10, "dynamic")
 end
