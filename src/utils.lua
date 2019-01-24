d4013c50791134ff2aa85694d61278bf681736aa
Skye Bleed <35306846+ithinkthisisarc@users.noreply.github.com>
2019-01-24 17:16:22 -0500

    Add files via upload

--- src/utils.lua
+++ src/utils.lua
@@ -1,18 +1,26 @@
 utils = {}
 
+bullet_speed = 7
+
 function utils.draw_bullet(bullet)
     if bullet.dire == "n" then
-        bullet.y = bullet.y - 5
+        bullet.y = bullet.y - bullet_speed
         love.graphics.circle("fill", bullet.x, bullet.y, 7, 100)
     elseif bullet.dire == "s" then
-        bullet.y = bullet.y + 5
+        bullet.y = bullet.y + bullet_speed
         love.graphics.circle("fill", bullet.x, bullet.y, 7, 100)
     elseif bullet.dire == "e" then
-        bullet.x = bullet.x + 5
+        bullet.x = bullet.x + bullet_speed
         love.graphics.circle("fill", bullet.x, bullet.y, 7, 100)
     elseif bullet.dire == "w" then
-        bullet.x = bullet.x - 5
+        bullet.x = bullet.x - bullet_speed
         love.graphics.circle("fill", bullet.x, bullet.y, 7, 100)
+    end
+end
+
+function utils.draw_sword(dire)
+    if dire == "n" then
+        love.graphics.polygon("fill", cX, cY, cX+5, cY+5, cX+50, cY+50)
     end
 end
 
